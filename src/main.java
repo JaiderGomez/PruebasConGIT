7c5b518a4cfe9e07d7edb17e1bfb60f1366e2a5c
Jaider Gomez <jdgomez231@misena.edu.co>
2022-08-13 09:05:23 -0500

    Realizo mi segundo commit, he modificado el archivo main: agregue mi nombre y donde estudio

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 public class main {
 
     public static void main(String[] args) {
-        System.out.println("como estas");
+        System.out.println("Hola Mundo");
+        System.out.println("Mi nombre es: Jaider Gomez Ortega");
+        System.out.println("Estudiante del programa MISION TIC 2022, Universidad de Antioquia");
     }
 }
