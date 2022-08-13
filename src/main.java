ac7298dc09002d037391523ca065ec33440913cf
Jaider Gomez <jdgomez231@misena.edu.co>
2022-08-13 10:26:24 -0500

    He modificado el archivo main desde otra PC

--- src/main.java
+++ src/main.java
@@ -4,5 +4,6 @@ public class main {
         System.out.println("Hola Mundo");
         System.out.println("Mi nombre es: Jaider Gomez Ortega");
         System.out.println("Estudiante del programa MISION TIC 2022, Universidad de Antioquia");
+        System.out.println("Este repositorio es para hacer prubas con git & github ");
     }
 }
